fec2fd43c96dc418c2d0a05ae6ee41266db793b4
ZhiyongG08 <ZHiyongg2789@nycstudents.net>
2025-05-12 10:11:06 -0400

    Final PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,9 +1,12 @@
-// Part A
+//PART A
 public int walkDogs(int hour) {
+        
+    int numDogs = company.numAvailableDogs(hours);
 
-
-
-
-
-  
-  }
+    if(maxDogs > numDogs) {
+        company.updateDogs(hour, numDogs);
+          return numDogs;
+    } else {
+        company.updateDogs(hour, maxDogs);
+        return maxDogs;
+    }
