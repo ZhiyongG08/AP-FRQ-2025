c4b8c7618d306e13cf39b90b67ac8494856567c3
ZhiyongG08 <ZHiyongg2789@nycstudents.net>
2025-05-14 22:46:05 -0400

    Final signedText.java

--- Q2/signedText.java
+++ Q2/signedText.java
@@ -1,1 +1,30 @@
+public class SignedText{
+  private String firstN;
+  private String lastN;
 
+  public SignedText(String fN, Strinf lN){
+    firstN = fN;
+    lastN = ln;
+  }
+
+  public String getSignature(){
+      if(fN.equals(""){
+        return lN;
+      }
+      return fN.substring(0,1) + "-" + lN;
+  }
+  public String addSignature(String text){
+    String r = "";
+    if(getSignature().indexOF(text) == -1){
+        r = text + getSignature();
+    }
+    if(text.indexOf(getSignature()) >= 0){
+        r = text;
+    }
+
+    if(text.indexOf(getSignature()) == 0){
+      r = text.substring(getSignature().length() + getSignature();
+    }
+    return r;
+  }
+}
